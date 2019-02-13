96b443cdaf3b6e681bf970e16b60e643367b72dc
Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
2019-02-13 15:37:15 +0000

    mcuboot: hard code flash map
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>

--- subsys/storage/flash_map/flash_map_default.c
+++ subsys/storage/flash_map/flash_map_default.c
@@ -8,14 +8,33 @@
 #include <zephyr.h>
 #include <flash_map.h>
 
-#define FLASH_AREA_FOO(i, _)				\
-	{.fa_id = i,					\
-	 .fa_off = DT_FLASH_AREA_##i##_OFFSET,		\
-	 .fa_dev_name = DT_FLASH_AREA_##i##_DEV,	\
-	 .fa_size = DT_FLASH_AREA_##i##_SIZE,},
-
 const struct flash_area default_flash_map[] = {
-	UTIL_LISTIFY(DT_FLASH_AREA_NUM, FLASH_AREA_FOO, ~)
+	{
+		.fa_id = 0,
+		.fa_off = DT_FLASH_AREA_MCUBOOT_OFFSET,
+		.fa_dev_name = DT_FLASH_AREA_0_DEV,
+		.fa_size = DT_FLASH_AREA_MCUBOOT_SIZE,
+	},{
+		.fa_id = 1,
+		.fa_off = DT_FLASH_AREA_IMAGE_0_OFFSET,
+		.fa_dev_name = DT_FLASH_AREA_0_DEV,
+		.fa_size = DT_FLASH_AREA_IMAGE_0_SIZE,
+	},{
+		.fa_id = 2,
+		.fa_off = DT_FLASH_AREA_IMAGE_1_OFFSET,
+		.fa_dev_name = DT_FLASH_AREA_0_DEV,
+		.fa_size = DT_FLASH_AREA_IMAGE_1_SIZE,
+	},{
+		.fa_id = 3,
+		.fa_off = DT_FLASH_AREA_IMAGE_SCRATCH_OFFSET,
+		.fa_dev_name = DT_FLASH_AREA_0_DEV,
+		.fa_size = DT_FLASH_AREA_IMAGE_SCRATCH_SIZE,
+	},{
+		.fa_id = 4,
+		.fa_off = DT_FLASH_AREA_STORAGE_OFFSET,
+		.fa_dev_name = DT_FLASH_AREA_0_DEV,
+		.fa_size = DT_FLASH_AREA_STORAGE_SIZE,
+	},
 };
 
 const int flash_map_entries = ARRAY_SIZE(default_flash_map);
